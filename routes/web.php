21a1215d33f715652ea141d5d44432db1db26cb1
Eng-AkramShaker <akramaboubakersh@gmail.com>
2025-01-10 17:58:58 +0200

    10/1

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,13 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Mcamara\LaravelLocalization\Facades\LaravelLocalization;
 
-Route::get('/', function () {
-    return view('welcome');
+
+
+
+Route::group(['prefix' => LaravelLocalization::setLocale()], function () {
+    Route::get('/', function () {
+        return view('empty');;
+    });
 });
